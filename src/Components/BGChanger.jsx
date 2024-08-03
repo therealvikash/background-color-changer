cfb1fcc3286be85e533e7aaad6caa510d0c57769
Bikash Yadav <bikashjadav333@gmail.com>
2024-08-03 15:35:05 +0530

    first commit

---
+++ src/Components/BGChanger.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function BGChanger() {
+  const [color, setColor] = useState("olive");
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            onClick={() => setColor("red")}
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            onClick={() => setColor("green")}
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            onClick={() => setColor("Orange")}
+            style={{ backgroundColor: "orange" }}
+          >
+            Orange
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BGChanger;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BGChanger from "./Components/BGChanger";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BGChanger />
+  </React.StrictMode>
+);
